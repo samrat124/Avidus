d93546488a18c4ce893f536ed00544afc6557cac
samrat kumar <sam9999950@gmail.com>
2023-08-12 13:57:54 +0530

    working on servers

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
   property: { type: mongoose.Schema.Types.ObjectId, ref: 'Property', required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the User model
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: true },
   // Other booking fields
