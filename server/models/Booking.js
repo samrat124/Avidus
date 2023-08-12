a624c90f3bdafc9672cf7c6f8dabf254a442eee3
samrat kumar <sam9999950@gmail.com>
2023-08-12 14:05:05 +0530

    working on servers

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
   property: { type: mongoose.Schema.Types.ObjectId, ref: 'Property', required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the User model
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: true },
   // Other booking fields
--- server/routes/property.js
+++ server/routes/property.js
@@ -77,19 +77,22 @@ router.get('/:propertyId', async (req, res) => {
     res.status(500).json({ error: 'An error occurred' });
   }
 });
-router.post('/:propertyId/book', authMiddleware, async (req, res) => {
+router.post('/:propertyId/book', async (req, res) => {
   try {
-    const property = await Property.findById(req.params.propertyId);
+    const propertyId = req.params.propertyId;
+    const property = await Property.findById(propertyId);
+
     if (!property) {
       return res.status(404).json({ error: 'Property not found' });
     }
+
     const { startDate, endDate } = req.body;
     const booking = new Booking({
       property: property._id,
-      user: req.user.userId, // Use the user ID from the authenticated request
       startDate,
       endDate,
     });
+
     await booking.save();
     res.status(201).json({ message: 'Booking successful' });
   } catch (error) {
